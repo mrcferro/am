eb2f1d9141f69205007dbd5e266aca36b213eceb
Marcio Roberio <mrcferro@gmail.com>
2017-10-26 12:36:50 -0300

    Classificador com janela de parzen finalizado.

--- parzen/parzenFunction.py
+++ parzen/parzenFunction.py
@@ -1,17 +1,18 @@
-# Marcio Roberio, Marcelo, Paulo 
 #
-# como usar   (x, indiceColunaFeature, pdMatriz, arrayH):
-# 1 - armazene em um array numpy o valor das janelas, chamando a função encontrarHDimensoes(pandasMatriz)
-# 2 - faça a chamada da função parzenMultivariadoProduto passando 
-#      a) o valor X que você deseja usar para calcular o valor na função densidade
-#      b) o índice da coluna correspondente à feature observada
-#      c) a matriz (pandas)
-#      d) o array, resultado da chamada da função encontrarHDimensoes(pandasMatriz) (item 1).
-
+# como usar   
+# parzen(features, pdMatriz)
+#     onde
+#       - features = array contendo os dados que serao classificados
+#       - pdMatriz = matriz (pandas) com os dados do dataset (colunas = features)
+#
 
 import numpy as np
 import pandas as pd
 import math
+
+
+## Arquivo parzenWindow.py. 
+#esse codigo está aqui apenas para evitar o problema de cache.. vai sair 
 
 
 # assumindo que a distribuição é gaussiana, o calculo de H = 1.06 * desvioPadrao * numeroExemplosTreino^-1/5
@@ -24,15 +25,16 @@ def encontrarH(npColuna):
 
 # recebe uma matriz de dados e encontra a largura da janela (h) para cada coluna (característica)
 # retorna um array contendo os coeficiente, onde cada elemento i representa o H da coluna j da matriz
-def encontrarHDimensoes(matriz):
+def encontrarHDimensoes(matrizNumpy):
     hDimensoes = []
-    colunas = len(matriz[0])
+    colunas = len(matrizNumpy[0])
     # para cada coluna...
     for i in range(colunas):
-        npColuna = np.array(matriz[:,i:i+1])
-        h = encontrarH(npColuna)
+        coluna = matrizNumpy[:,i:i+1]
+        h = encontrarH(coluna)
         hDimensoes.append(h)
     return hDimensoes
+
 
 
 # função kernel univariada normal - slide "Tecnicas nao parametricas", pg 13    
@@ -40,37 +42,70 @@ def funcaoKernelUnivariadaNormal(u):
     return (1.0/ math.sqrt(2 * math.pi)) * math.exp(- (u**2)/2)
 
 
+
 # features = array com os valores para o qual deseja-se encontrar o valor da função densidade em uma determinada classe.
 #            o numero de elementos deve ser igual ao numero de colunas da matriz
 # indiceColunaFeature = feature observada (coluna da matriz)
-# pdMatriz = matriz (pandas) que contem todas as features(colunas) das amostras(linhas) de uma determinada classe
+# matriz = matriz que contem todas as features(colunas) das amostras(linhas) de uma determinada classe
 # arrayH - array contendo o valor H (janela) para todas as colunas da matriz. A função encontrarHDimensoes() faz isso.
-
-def parzenMultivariadoProduto(features, pdMatriz, arrayH):
-    n = len(pdMatriz)
+def parzenMultivariadoProduto(features, matrizNumpy, arrayH):
+    n = len(matrizNumpy)
     soma = 0
     # p = numero de colunas, dimensoes
-    p = len(pdMatriz[0])  
-   
+    p = len(matrizNumpy[0])
+    
+    # o numero das linhas podem estar desordenadas, por causa do shurfle
+    numLinha = 0
+    
     for i in range(n):
         produto = 1
-        for j in range(p):
-            coluna = np.array(pdMatriz[ : , j:j+1])
+        for j in range(p):            
             h = arrayH[j]
-            u = (features[j] - pdMatriz[i][j])/h 
+            Xij = matrizNumpy[i][j]
+            u = (features[j] - Xij)/h
+            #print("i="+str(i)+", j="+str(j)+ ". u= (" + str(features[j]) + " - " + str(Xij) + ")/" + str(h) + " = " + str(u) )
             kernel = funcaoKernelUnivariadaNormal(  u )
             produto = produto * kernel
         soma = soma + produto
-    
     # dividido pela media do H
-    
     resultado = (1.0/n) * (1.0/np.mean(arrayH)) * soma  
     return resultado
 
 
 
-# gera grafico da densidade
-def graficoParzen(pdMatriz, coluna, arrayH):
+# função principal
+# parametros:
+#      1 - features: array contendo os valores das features que serão analisadas na função densidade
+#      2 - matriz de dados contendo em suas colunas as features do dataset
+def parzen(features, pdMatriz):
+    
+    f = np.array(features)
+    m = np.array(pdMatriz) 
+    
+    #print("parzen, features=" + str(f.shape) + ", dados=" + str(m.shape))
+    
+    arrayH = encontrarHDimensoes(m)
+    result = parzenMultivariadoProduto(f, m, arrayH)
+    return result
+
+
+def testeGraficoParzen(indiceColuna, pdMatriz):
+    arrayH = encontrarHDimensoes(pdMatriz)
+    print("H (janelas) = \n" + str(arrayH))
+    print("Média H = " + str(np.mean(arrayH)))
+    graficoDensidade(pdMatriz, indiceColuna, arrayH)
+    print("---------------------------------------")
+    print("Coluna " + str(indiceColuna) + " \n---------------------------------------")
+    print("Quantidade de elementos: " + str(len(pdMatriz[:,indiceColuna])))
+    print("Dados (primeiros 50 elementos):\n " + str(pdMatriz[:50,indiceColuna]))
+    print("Menor Valor = " + str(min(pdMatriz[:,indiceColuna])))
+    print("Maior Valor = " + str(max(pdMatriz[:,indiceColuna])))
+    print("Média  = " + str(np.mean(pdMatriz[:,indiceColuna])))
+    print("Mediana  = " + str(np.median(pdMatriz[:,indiceColuna])))
+    print("Janela (parzen) = " + str(arrayH[indiceColuna]))
+    print("\n\n")
+
+def graficoDensidade(pdMatriz, coluna, arrayH):
     dados = pdMatriz[:,coluna:coluna+1]    
     janela = arrayH[coluna]
 
@@ -104,21 +139,3 @@ def graficoParzen(pdMatriz, coluna, arrayH):
     f['densidade'] = densidade
     texto = "Feature = " + str(coluna)
     f.plot(x='x',y='densidade', label=texto)
-
-
-def testeGraficoParzen(indiceColuna, pdMatriz):
-    arrayH = encontrarHDimensoes(pdMatriz)
-    print("H (janelas) = \n" + str(arrayH))
-    print("Média H = " + str(np.mean(arrayH))
-    graficoParzen(pdMatriz, indiceColuna, arrayH)
-    print("---------------------------------------")
-    print("Coluna " + str(indiceColuna) + " \n---------------------------------------")
-    print("Quantidade de elementos: " + str(len(pdMatriz[:,indiceColuna])))
-    print("Dados (primeiros 50 elementos):\n " + str(pdMatriz[:50,indiceColuna]))
-    print("Menor Valor = " + str(min(pdMatriz[:,indiceColuna])))
-    print("Maior Valor = " + str(max(pdMatriz[:,indiceColuna])))
-    print("Média  = " + str(np.mean(pdMatriz[:,indiceColuna])))
-    print("Mediana  = " + str(np.median(pdMatriz[:,indiceColuna])))
-    print("Janela (parzen) = " + str(arrayH[indiceColuna]))
-    print("\n\n")
-
